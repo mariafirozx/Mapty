40e7dafdaf7df5d3ea4ee4fc0c3627859343f765
Maria <mariafiroz9@gmail.com>
2025-02-10 03:42:31 +0800

    NEW- user_valid

--- mapController.js
+++ mapController.js
@@ -51,7 +51,7 @@ const clearAllCard = document.querySelector('.deleteAllCard');
 const overlay = document.querySelector('.overlay');
 const confirmBtn = document.querySelector('.confirmBtn');
 const noBtn = document.querySelector('.noBtn');
-const logBtn = document.querySelector('.login');
+const logBtn = document.getElementById('login-btn');
 const regBtn = document.getElementById('register-btn');
 
 const loginForm = document.getElementById('form-log');
@@ -90,7 +90,8 @@ class Workout{
 
     _setDescription(){
         // prettier-ignore
-        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        const months = 
+        ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
         this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${months[this.date.getMonth()]} ${this.date.getDate()}`;
     }
@@ -151,8 +152,10 @@ class App{
     #workouts = [];
     #editWorkout = null;
     constructor(){
+        this._renderErrorCard();
 
         this._registerRender();
+        this._loginRender();
 
 
         // if(regForm){
@@ -239,11 +242,12 @@ class App{
                         
                         
                     }else{
-                        this._userReg();
+
+                        
+                        this._userValid();
                         
                         // this.toggleWindow('RenderSpinner'); //show spinner
 
-                        wrapper.classList.remove('active');
 
                         // logRegPage.classList.toggle('hide');
                         // main.classList.toggle('hide');
@@ -260,6 +264,21 @@ class App{
 
     _loginRender(){
 
+        try{
+
+            logBtn.addEventListener('click', e=>{
+                e.preventDefault();
+                if(!username.value || !password.value){
+                    alert('Please fill in all fields login');
+                }else{
+                    this._userAuth();
+                }
+
+            })
+
+        }catch(err){
+            console.error(err);
+        }
         // let user = 'zahra';
         // let pass = '786786';
 
@@ -295,7 +314,11 @@ class App{
 
 
         if (!email || !password || !username) {
-            alert('Please fill in all fields.');
+            alert('Please fill in all fields. register');
+            return;
+        }
+        if(password.length < 6){
+            alert('Password must be at least 6 characters long');
             return;
         }
 
@@ -332,21 +355,151 @@ class App{
 }
 
    async _userAuth(){
-    //     let {data, error} = await supabase.auth.signUp({
+    try{
 
-    //         email: username.value,
-    //         password: password.value
+        const email = username.value;
+        const pass = password.value;
+        
+        const {data: userAuth, error} = await supabase.auth.signInWithPassword({
+            email: email,
+            password: pass
+        
+        })
 
-    // })
-    //     if(error) console.error(error);
-    //     console.log(data);
+        if (error) {
+            alert('Invalid username or password');
+        }else{
 
-    //     //create user database //INSERT 
-    // const {err} = await supabase
-    // .from('users')
-    // .insert({email: username.value, password: password.value})
+            logRegPage.classList.toggle('hide');
+            main.classList.toggle('hide');
+            //fetch username
+            const {data: user} = await supabase
+                .from('users')
+                .select('username')
+                .eq('email', userAuth.user.email)
+                .single();
+        
+            if(user){
+                alert(`Welcome ${user.username}`);
+            }
+        }
+
+    }catch(err){
+        console.error(err);
+    }
 
  }
+
+    async _getUser(){
+        try{
+            const {data: {user}} = await supabase.auth.getUser();
+             return user.id;
+
+        }catch(err){
+
+        }
+
+    }
+
+    async _userValid(){
+        try{
+
+            //if user alr exist -> show email already registered msg
+            const {data, err} = await supabase
+            .from('users')
+            .select('email')
+            .eq('email', regEmail.value);
+
+            
+            if(data && data.length > 0){
+                console.log(data);
+                alert('Email already registered');
+                
+            }
+           
+            else{
+                this._userReg();
+                wrapper.classList.remove('active');
+
+            }
+
+        }catch(err){
+
+        }
+    }
+
+    _renderErrorCard(){
+        //change card inner html
+        //show card
+        //hide card after 2s
+
+        // const path = document.getElementById('path');
+        // const path2 = document.getElementById('path2');
+        // document.querySelector('.message-text').textContent = 'Email Already Exists';
+        // document.querySelector('.sub-text').textContent = 'Please try using another email.';
+
+        // path.innerHTML = 'd="M0,256L11.4,240C22.9,224,46,192,69,192C91.4,192,114,224,137,234.7C160,245,183,235,206,213.3C228.6,192,251,160,274,149.3C297.1,139,320,149,343,181.3C365.7,213,389,267,411,282.7C434.3,299,457,277,480,250.7C502.9,224,526,192,549,181.3C571.4,171,594,181,617,208C640,235,663,277,686,256C708.6,235,731,149,754,122.7C777.1,96,800,128,823,165.3C845.7,203,869,245,891,224C914.3,203,937,117,960,112C982.9,107,1006,181,1029,197.3C1051.4,213,1074,171,1097,144C1120,117,1143,107,1166,133.3C1188.6,160,1211,224,1234,218.7C1257.1,213,1280,139,1303,133.3C1325.7,128,1349,192,1371,192C1394.3,192,1417,128,1429,96L1440,64L1440,320L1428.6,320C1417.1,320,1394,320,1371,320C1348.6,320,1326,320,1303,320C1280,320,1257,320,1234,320C1211.4,320,1189,320,1166,320C1142.9,320,1120,320,1097,320C1074.3,320,1051,320,1029,320C1005.7,320,983,320,960,320C937.1,320,914,320,891,320C868.6,320,846,320,823,320C800,320,777,320,754,320C731.4,320,709,320,686,320C662.9,320,640,320,617,320C594.3,320,571,320,549,320C525.7,320,503,320,480,320C457.1,320,434,320,411,320C388.6,320,366,320,343,320C320,320,297,320,274,320C251.4,320,229,320,206,320C182.9,320,160,320,137,320C114.3,320,91,320,69,320C45.7,320,23,320,11,320L0,320Z" fill-opacity="1"';
+        // path2.innerHTML ='d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c-9.4 9.4-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47c-9.4-9.4-24.6-9.4-33.9 0z"';
+        // card.classList.remove('hide');
+
+        const el = `
+            <svg class="wave" viewBox="0 0 1440 320" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M0,256L11.4,240C22.9,224,46,192,69,192C91.4,192,114,224,137,234.7C160,245,183,235,206,213.3C228.6,192,251,160,274,149.3C297.1,139,320,149,343,181.3C365.7,213,389,267,411,282.7C434.3,299,457,277,480,250.7C502.9,224,526,192,549,181.3C571.4,171,594,181,617,208C640,235,663,277,686,256C708.6,235,731,149,754,122.7C777.1,96,800,128,823,165.3C845.7,203,869,245,891,224C914.3,203,937,117,960,112C982.9,107,1006,181,1029,197.3C1051.4,213,1074,171,1097,144C1120,117,1143,107,1166,133.3C1188.6,160,1211,224,1234,218.7C1257.1,213,1280,139,1303,133.3C1325.7,128,1349,192,1371,192C1394.3,192,1417,128,1429,96L1440,64L1440,320L1428.6,320C1417.1,320,1394,320,1371,320C1348.6,320,1326,320,1303,320C1280,320,1257,320,1234,320C1211.4,320,1189,320,1166,320C1142.9,320,1120,320,1097,320C1074.3,320,1051,320,1029,320C1005.7,320,983,320,960,320C937.1,320,914,320,891,320C868.6,320,846,320,823,320C800,320,777,320,754,320C731.4,320,709,320,686,320C662.9,320,640,320,617,320C594.3,320,571,320,549,320C525.7,320,503,320,480,320C457.1,320,434,320,411,320C388.6,320,366,320,343,320C320,320,297,320,274,320C251.4,320,229,320,206,320C182.9,320,160,320,137,320C114.3,320,91,320,69,320C45.7,320,23,320,11,320L0,320Z"
+      fill-opacity="1"
+    ></path>
+  </svg>
+
+  <div class="icon-container">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 512 512"
+      stroke-width="0"
+      fill="currentColor"
+      stroke="currentColor"
+      class="icon"
+    >
+      <path
+        d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c-9.4 9.4-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47c-9.4-9.4-24.6-9.4-33.9 0z"
+      ></path>
+    </svg>
+  </div>
+  <div class="message-text-container">
+    <p class="message-text">Email Already Exist!</p>
+    <p class="sub-text">Please register using another email.</p>
+  </div>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 15 15"
+    stroke-width="0"
+    fill="none"
+    stroke="currentColor"
+    class="cross-icon"
+  >
+    <path
+      fill="currentColor"
+      d="M11.7816 4.03157C12.0062 3.80702 12.0062 3.44295 11.7816 3.2184C11.5571 2.99385 11.193 2.99385 10.9685 3.2184L7.50005 6.68682L4.03164 3.2184C3.80708 2.99385 3.44301 2.99385 3.21846 3.2184C2.99391 3.44295 2.99391 3.80702 3.21846 4.03157L6.68688 7.49999L3.21846 10.9684C2.99391 11.193 2.99391 11.557 3.21846 11.7816C3.44301 12.0061 3.80708 12.0061 4.03164 11.7816L7.50005 8.31316L10.9685 11.7816C11.193 12.0061 11.5571 12.0061 11.7816 11.7816C12.0062 11.557 12.0062 11.193 11.7816 10.9684L8.31322 7.49999L11.7816 4.03157Z"
+      clip-rule="evenodd"
+      fill-rule="evenodd"
+    ></path>
+  </svg>
+        
+        `
+
+        card.innerHTML ='';
+        card.insertAdjacentHTML('afterbegin', el);
+        card.classList.remove('hide');
+        
+        
+        // card.innerHTML = el;
+        //clear
+        
+        // card.classList.remove('hide');
+
+    }
+
+
+ ////////////////////////////--WILL SEPERATE MAP VIEW & AUTH/////////////////////////////////////////////
     _getPosition(){
 
         //using bind since this keyword is undefined in a reg function, and loadMap here is reg function
@@ -412,7 +565,7 @@ class App{
 
     //generate new workout form -- render in map and render in list
 
-    _newWorkout(e){
+    async _newWorkout(e){
         e.preventDefault();
     
         const validInput = (...inputs) => inputs.every(inp => Number.isFinite(inp) );
@@ -496,9 +649,19 @@ class App{
         this._renderWorkout(workout);
 
         this._hideform();
+
+        //set workouts to supabase
+
+
         
         //set local storage to all workouts
         this._setLocalStorage();
+        //insert details in supabase
+        // const userId = await this._getUser();
+        // console.log(userId);
+        this._setSupbaseWorkouts(workout);
+        // console.log(workout.type, workout.distance, workout.duration, workout.date,userId);
+
     }
 
 
@@ -774,19 +937,61 @@ class App{
     }
 
 
+    async _setSupbaseWorkouts(workout){
+        try{
+
+            const userId = await this._getUser();
+
+            if(workout.type === 'running'){
+                const {data, err} = await supabase
+                .from('workout')
+                .insert([{
+                    user_id: userId,
+                    coords: JSON.stringify(workout.coords),
+                    workoutType: workout.type, 
+                    workoutDistance: workout.distance,
+                    workoutDuration: workout.duration,
+                    workoutCadence: workout.cadence,
+                    workoutDate: workout.date.toISOString(),
+                    
+                }]);
+
+
+                if(err){
+                    alert('Workout not saved');}
+            }
+
+            if(workout.type === 'cycling'){
+                const {data, err} = await supabase
+                .from('workout')
+                .insert([{
+                    user_id: userId,
+                    coords: JSON.stringify(workout.coords),
+                    workoutType: workout.type, 
+                    workoutDistance: workout.distance,
+                    workoutDuration: workout.duration,
+                    workoutElevation: workout.elevation,
+                    workoutDate: workout.date.toISOString(),
+                }]);
+                if(err){
+                    alert('Workout not saved');
+                }else{
+                    console.log(data);
+                }
+            }
+        
+        }catch(err){
+            console.error(err);
+            alert('Workout not saved');
+        }
+    }
+
+
     _setLocalStorage(){
         //a very simple API --> only to be used small data, not large data...
       localStorage.setItem('workout', JSON.stringify(this.#workouts));
     }
-    // deleteworkout(){
-    //     const workEl = e.target.closest('.workout');
-
-    //     if(!workEl) return;
-
-        
-
-
-    // }
+   
     reset(){
 
         localStorage.removeItem('workout');
